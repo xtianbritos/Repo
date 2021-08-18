6e0d1a1f9d417651f459e242078670f3b9e944d3
xtianbritos <xtiancuenta1@gmail.com>
2021-08-18 13:28:26 -0300

    Con cambiios pero sin mostrar el listado

--- js/login.js
+++ js/login.js
@@ -4,3 +4,38 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+function Verificar() {
+    let usuario = document.getElementById('user');
+    let contrasena = document.getElementById('password');
+    let mensaje = document.getElementById('msj');
+    let Usuario ={};
+
+    if (user.value.trim() === "" || password.value.trim() === "") {
+
+        usuario.classList.add("Invalid");
+
+        mensaje.innerHTML = "Dato requerido";
+        mensaje.classList.add("Invalid");
+
+        contrasena.classList.add("Invalid");
+    }else {
+
+        usuario.classList.remove("Invalid");
+        contrasena.classList.remove("Invalid");
+
+        usuario.classList.add ("Valid");
+        Usuario.nombre = user.value;
+        Usuario.estado = "Conectado";
+
+        mensaje.classList.remove("Invalid");
+        mensaje.classList.add("Valid");
+        mensaje.innerHTML = "Conectado";
+
+        localStorage.setItem('Usuario', JSON.stringify(Usuario));
+        sessionStorage.setItem('Usuario', JSON.stringify(Usuario));
+
+        contrasena.classList.add ("Valid")
+        location.href="index.html";
+    }
+}
--- js/products.js
+++ js/products.js
@@ -1,6 +1,44 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
+let categoriesArray = [];
+
+function showCategoriesList(array){
+
+  let htmlContentToAppend = "";
+  for(let i = 0; i < array.length; i++){
+      let categories = array[i];
+
+      htmlContentToAppend += `
+      <div class="list-group-item list-group-item-action">
+          <div class="row">
+              <div class="col-3">
+                  <img src="` + categories.imgSrc + `" alt="` + categories.description + `" class="img-thumbnail">
+              </div>
+              <div class="col">
+                  <div class="d-flex w-100 justify-content-between">
+                      <h4 class="mb-1">`+ categories.name +`</h4>
+                      <small class="text-muted">` + categories.soldCount + ` artículos</small>
+                  </div>
+                      <p class="mb-1">`+ categories.description +`</p>
+              </div>
+          </div>
+      </div>
+      `
+      document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+  }
+  
+  
+  //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener("DOMContentLoaded", function(e){
+  
+  getJSONData(PRODUCTS_URL).then(function(resultObj){
 
-});
+      if (resultObj.status === "ok") {
+          categoriesArray = resultObj.data;
+          //Muestro las categorías ordenadas
+          showCategoriesList(categoriesArray);
+          c
+      }
+    })
+  })
+}
