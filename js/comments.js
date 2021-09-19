4014a5aea89180af492236392554d2efb2a93896
xtianbritos <xtiancuenta1@gmail.com>
2021-09-19 19:43:42 -0300

    Entrega 3

--- js/comments.js
+++ js/comments.js
@@ -1,18 +1,33 @@
 let persona = {};
 
 document.addEventListener("DOMContentLoaded", () => {
+
 	
 	 document.getElementById("agregar").addEventListener("click", () => {
+
+		if (document.getElementById("nombre").value === ""){
+			persona.nombre = "Anónimo";
+		}else {
+			persona.nombre = document.getElementById("nombre").value; 
+		}
+		if (document.getElementById("comentario").value === "") {
+			persona.comentario = "Sin comentario.";
+		} else {
+			persona.comentario = document.getElementById("comentario").value;
+		}
+		persona.puntos = document.getElementById("valor").innerHTML;
+				
+		let date=new Date(); 
+		persona.hora=date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " +date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+				
+		mostrar(persona);
+				
+		document.getElementById("nombre").value = "";
+		document.getElementById("comentario").value = "";
+		document.getElementsByName("estrellas").forEach( radio => { if (radio.checked == true){radio.checked = false}});
+		document.getElementById("valor").innerHTML ="";
   
-	  persona.nombre = document.getElementById("nombre").value; 
-	  persona.edad = document.getElementById("edad").value;
-	  persona.puntos = document.getElementById("valor").innerHTML;
 	  
-	  let date=new Date(); 
-	  let mes = date.getMonth()+1;
-	  persona.hora=date.getFullYear() + "-" + mes + "-" + date.getDate() + " " +date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-	
-	  mostrar(persona);
 	});
   });
   
@@ -28,7 +43,7 @@ document.addEventListener("DOMContentLoaded", () => {
 					<small class="text-muted">` + persona.hora + `</small>
 				</div>
 				<small class="text-muted">` + score(persona.puntos) + `</small>
-				<p class="mb-1">`+ persona.edad +`</p>
+				<p class="mb-1">`+ persona.comentario +`</p>
 			</div>
 		</div>
 	</div>
